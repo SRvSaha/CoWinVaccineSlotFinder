d28fb691efca9788da7d9741839aee4df4b6554e
SRvSaha <contact.srvsaha@gmail.com>
2021-05-23 16:24:34 +0530

    Enhanced #41 by managing AuthToken Automatically without user intervention.

--- CoWin.Core/Auth/OTPAuthenticator.cs
+++ CoWin.Core/Auth/OTPAuthenticator.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Security.Cryptography;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.IO;
 
 namespace CoWin.Auth
 {
@@ -18,6 +19,7 @@ namespace CoWin.Auth
         private IConfiguration _configuration;
         public static string BEARER_TOKEN;
         private bool isOTPEntered = false;
+        private string authTokenFilename = "authToken.json";
         public OTPAuthenticator(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -33,14 +35,15 @@ namespace CoWin.Auth
                 endpoint = _configuration["CoWinAPI:Auth:OTPGeneratorUrl"];
             }
 
-            if (!string.IsNullOrEmpty(_configuration["CoWinAPI:Auth:BearerToken"]))
+            var token = GetBearerToken();
+            if (!string.IsNullOrEmpty(token))
             {
                 // Check if user already has a valid bearer token, if yes use it.
-                if (IsValidBearerToken(_configuration["CoWinAPI:Auth:BearerToken"]))
+                if (IsValidBearerToken(token))
                 {
-                    BEARER_TOKEN = _configuration["CoWinAPI:Auth:BearerToken"];
+                    BEARER_TOKEN = token;
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"[INFO] Resuming Session for Mobile No: {_configuration["CoWinAPI:Auth:Mobile"]} at {DateTime.Now} using the provided Bearer Token in config file");
+                    Console.WriteLine($"[INFO] Resuming Session for Mobile No: {_configuration["CoWinAPI:Auth:Mobile"]} at {DateTime.Now} using the provided Bearer Token in auth.json config file");
                     Console.ResetColor();
                     return;
                 }
@@ -84,13 +87,14 @@ namespace CoWin.Auth
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     Console.WriteLine($"[INFO] User Validated with Mobile No {_configuration["CoWinAPI:Auth:Mobile"]} at {DateTime.Now}");
-                    
-                    BEARER_TOKEN = JsonConvert.DeserializeObject<OtpModel>(response.Content).BearerToken;
 
+                    var otpModel = JsonConvert.DeserializeObject<OtpModel>(response.Content);
+                    BEARER_TOKEN = otpModel.BearerToken;
+                    SetBearerToken(otpModel);
+                    var authTokenPath = Path.Combine(Directory.GetCurrentDirectory(), authTokenFilename);
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"[INFO] Your Bearer Token is \"{BEARER_TOKEN}\"");
+                    Console.WriteLine($"[INFO] Your Bearer Token is stored here: {authTokenPath} valid for only 15 minutes and will expire on {GetTokenExpiryDate(BEARER_TOKEN)} [Your BearerToken will be re-used till it's not expired, so you have seamless UX.]\n");
                     Console.ResetColor();
-                    Console.WriteLine($"[INFO] Your BearerToken is valid for only 15 minutes and will expire on {GetTokenExpiryDate(BEARER_TOKEN)} [In Case you want to re-use the Session, please copy the BearerToken, and put it in config file on subsequent run]\n");
                 }
                 else
                 {
@@ -103,6 +107,34 @@ namespace CoWin.Auth
             }
 
         }
+
+        private string GetBearerToken()
+        {
+            string fileName = authTokenFilename;
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            try
+            {
+                var data = File.ReadAllText(filepath);
+                var token = JsonConvert.DeserializeObject<OtpModel>(data);
+                return token.BearerToken;
+            }
+            catch(Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"[WARNING] AuthToken file is missing in your machine, so unable to resume your existing session: {e.Message} {e.StackTrace}");
+                Console.ResetColor();
+                return null;
+            }
+        }
+
+        private void SetBearerToken(OtpModel model)
+        {
+            string fileName = authTokenFilename;
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            var token = JsonConvert.SerializeObject(model);
+            File.WriteAllText(filepath, token);
+        }
+
 
         private bool IsValidBearerToken(string bearerToken)
         {
